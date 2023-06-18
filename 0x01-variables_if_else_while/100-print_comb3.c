0d7eaeac973634c79ed051fa9db1a8a482b6db38
David <niklausdavid30@gmail.com>
2023-06-18 07:11:23 -0700

    added first advanced task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,21 +4,29 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int d, f;
+	int d, p;
 
-    	for(d = '0'; d < '9'; d++)
-    	{
-        	for(f = d + 1; f <= '9'; f++)
-        	{
-            		putchar(d);
-            		putchar(f);
-            		putchar(',');
-            		putchar(' ');
-        	}
-    	}
-    	putchar('\n');
+	for (d = '0'; d < '9'; d++)
+	{
+		for (p = d + 1; p <= '9'; p++)
+		{
+			if (p != d)
+			{
+				putchar(d);
+				putchar(p);
+
+				if (d == '8' && p == '9')
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
 
 	return (0);
 }
