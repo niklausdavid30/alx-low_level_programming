e24360b55e9c45d3eed71d5c617a2d0e7b8a094e
David <niklausdavid30@gmail.com>
2023-06-20 18:14:51 -0700

    added task 0-11

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabe, in lowercase, followed by a new line
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_abs - Check Main
+ * @r: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: Absolut value of number r
+ */
+int _abs(int r)
+{
+	int n;
+
+	n = abs(r);
+
+	return (n);
+}
