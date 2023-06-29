9bd4f0a5a95bd622e568aa391bb700d53191e1a2
David Ani <100693342+niklausdavid30@users.noreply.github.com>
2023-06-29 13:42:53 +0100

    redid task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * *_strcat - function commute srtings
@@ -10,7 +9,24 @@
 
 char *_strcat(char *dest, char *src)
 {
-    strcat(dest, src);
+    int i;
+    int j;
 
+    i = 0;
+    j = 0;
+
+    while (dest[i] != '\0')
+    {
+        i++;
+    }
+
+    while (src[j] != '\0')
+    {
+        dest[i] = src[j];
+        j++;
+        i++;
+    }
+
+    dest[i] = '\0';
     return (dest);
 }
