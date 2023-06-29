8bbfbbf6f8e6cd02aa80f741130a95da9ebfe53b
David <niklausdavid30@gmail.com>
2023-06-29 06:28:42 -0700

    updated task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+
 /**
  * _strncat - concatenates two strings,
  * @dest: destination.
@@ -7,6 +8,7 @@
  * @n: amount of bytes used from src.
  * Return: the pointer to dest.
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	strncat(dest, src, n);
